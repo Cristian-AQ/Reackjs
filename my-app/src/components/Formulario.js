47ba110f52e2968378bf8d91c7a31723ba3b674b
Cristian-Aquino <yojar.aquino@unmsm.edu.pe>
2021-05-26 12:15:40 -0500

    validar_formulario

--- my-app/src/components/Formulario.js
+++ my-app/src/components/Formulario.js
@@ -1,6 +1,14 @@
 import React,{useState,Fragment} from 'react';
+import {useForm} from 'react-hook-form'//para la validacion
 
 const Formulario = () => {
+
+    const {register,handleSubmit,formState:{errors}} = useForm()
+    const onsubmit = (data,e) => {
+        console.log(data)
+        e.target.reset()
+    }
+
     const [datos,setDatos] = useState({
         nombre:'',
         apellido:''
@@ -46,6 +54,21 @@ const Formulario = () => {
                 </div>
             </form>
             <h3>{datos.nombre}-{datos.apellido}</h3>
+            <form onSubmit={handleSubmit(onsubmit)}>
+                <input
+                    name='titulo'
+                    className='form-control my-2'
+                    {...register('titulo',{
+                        required:{
+                            value:true, message:'Titulo requerido'
+                        }
+                    })}
+                />
+                <span className='text-danger text-small d-block mb2'>
+                    {errors.titulo && errors.titulo.message}
+                </span>
+                <button className='btn btn-primary'>Enviar</button>
+            </form>
         </Fragment> 
         
      );
